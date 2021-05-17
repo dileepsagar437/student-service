97a4122f65ee5874186d917933c90623aad21466
LENOVO <LENOVO@DESKTOP-LA2NCEQ>
2021-05-17 15:30:43 +0530

    first-commit

---
+++ student-services/src/main/java/com/in28minutes/springboot/controller/StudentController.java
@@ -0,0 +1,70 @@
+package com.in28minutes.springboot.controller;
+
+import java.net.URI;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.in28minutes.springboot.model.Course;
+import com.in28minutes.springboot.service.StudentService;
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	private StudentService studentService;
+
+	@GetMapping("/students/{studentId}/courses")
+	public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
+		return studentService.retrieveCourses(studentId);
+	}
+	
+	@GetMapping("/students/{studentId}/courses/{courseId}")
+	public Course retrieveDetailsForCourse(@PathVariable String studentId,
+			@PathVariable String courseId) {
+		return studentService.retrieveCourse(studentId, courseId);
+	}
+	
+	@PostMapping("/students/{studentId}/courses")
+	public ResponseEntity<Void> registerStudentForCourse(
+			@PathVariable String studentId, @RequestBody Course newCourse) {
+
+		Course course = studentService.addCourse(studentId, newCourse);
+
+		if (course == null)
+			return ResponseEntity.noContent().build();
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+				"/{id}").buildAndExpand(course.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+	
+	public class ExceptionHandlerController {
+
+	    public static final String DEFAULT_ERROR_VIEW = "error";
+
+	    @ExceptionHandler(value = {Exception.class, RuntimeException.class})
+	    public ModelAndView defaultErrorHandler(HttpServletRequest request, Exception e) {
+	            ModelAndView mav = new ModelAndView(DEFAULT_ERROR_VIEW);
+
+	        mav.addObject("datetime", new Date());
+	        mav.addObject("exception", e);
+	        mav.addObject("url", request.getRequestURL());
+	        return mav;
+	    }
+	}
+
+}
